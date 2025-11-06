c750a1b44276cb3c72b5148722c0e141ee751752
taiatiniyara <taiatiniyara@gmail.com>
2025-11-06 22:56:57 +1200

    eewd

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,9 +1,12 @@
 import { createAuthClient } from "better-auth/react";
-const url = process.env.BETTER_AUTH_URL || "http://localhost:3000";
+import { magicLinkClient } from "better-auth/client/plugins";
+
+const url = process.env.BETTER_AUTH_URL;
 if (!url) {
   throw new Error("BETTER_AUTH_URL is not defined");
 }
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: url,
+  baseURL: url + "/api/auth",
+  plugins: [magicLinkClient()],
 });
