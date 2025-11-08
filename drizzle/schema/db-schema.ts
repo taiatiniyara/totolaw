7406127738586cd404cbb9595d76018ff4d7ef5e
taiatiniyara <taiatiniyara@gmail.com>
2025-11-09 06:49:03 +1200

    vvdd

--- drizzle/schema/db-schema.ts
+++
@@ -1,7 +0,0 @@
-import { pgTable, text, uuid } from "drizzle-orm/pg-core";
-
-export const cases = pgTable("cases", {
-  id: uuid("id").primaryKey(),
-  title: text("title").notNull(),
-  description: text("description"),
-});
